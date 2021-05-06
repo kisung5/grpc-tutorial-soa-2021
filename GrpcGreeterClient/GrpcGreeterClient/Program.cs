fdf08fc0acd7aafd96b95e646090ac3b790a7394
Ki Sung Lim <kisunglim@estudiantec.cr>
2021-05-06 08:19:31 -0600

    Added gRPC service and client sample for tutorial

---
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var watch = new Stopwatch();
+            watch.Start();
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            watch.Stop();
+            var responseTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("The reply lasted: " + responseTime + " ms");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
